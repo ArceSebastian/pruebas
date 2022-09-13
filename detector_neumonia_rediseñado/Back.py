6d2f267b0993000656375b43c2abb7efad1a1bdb
ArceSebastian <103223152+ArceSebastian@users.noreply.github.com>
2022-09-13 14:42:38 -0500

    Update Back.py

--- detector_neumonia_rediseñado/Back.py
+++ detector_neumonia_rediseñado/Back.py
@@ -31,7 +31,7 @@ class backe:
             self.img1 = img2show.resize((250, 250), Image.ANTIALIAS)
             self.img1 = ImageTk.PhotoImage(self.img1)
             self.text_img1.image_create(END, image=self.img1)
-            self.button1["state"] = "enabled"
+            UI.button1["state"] = "enabled"
 
     def run_model(self):
             self.label, self.proba, self.heatmap = infer.predict(self.array)
