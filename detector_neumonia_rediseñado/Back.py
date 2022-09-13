20d1dc9e4a9fc681fb58be5daa8ecd5f07ed751c
ArceSebastian <103223152+ArceSebastian@users.noreply.github.com>
2022-09-13 14:39:28 -0500

    Update Back.py

--- detector_neumonia_rediseñado/Back.py
+++ detector_neumonia_rediseñado/Back.py
@@ -27,7 +27,7 @@ class backe:
             ),
         )
         if filepath:
-            self.array, img2show = Backend.read_jpg_file(filepath)
+            self.array, img2show = infer.read_jpg_file(filepath)
             self.img1 = img2show.resize((250, 250), Image.ANTIALIAS)
             self.img1 = ImageTk.PhotoImage(self.img1)
             self.text_img1.image_create(END, image=self.img1)
